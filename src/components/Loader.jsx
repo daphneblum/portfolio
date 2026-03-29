a8897b11a5fde7603e8ca4d47868db67ad1a59ee
daphneblum <152042198+daphneblum@users.noreply.github.com>
2026-03-29 16:21:57 -0500

    Add mobile scaling

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,15 @@ const CanvasLoader = () => {
   const { progress } = useProgress();
   return (
     <Html center>
-      <span>{progress.toFixed(2)}%</span>
+      <span className="canvas-load"></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: '#f1f1f1',
+          fontWeight: 800,
+          marginTop: 40
+        }}
+      >{progress.toFixed(2)}%</p>
     </Html>
   )
 }
