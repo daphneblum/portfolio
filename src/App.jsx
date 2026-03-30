61f3016124b7338e2b06c36eaf9bc8bbc20f5765
daphneblum <152042198+daphneblum@users.noreply.github.com>
2026-03-30 02:48:11 -0500

    Add scroll bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useEffect(() => {
+    let timeout;
+
+    const handleScroll = () => {
+      document.body.classList.add('scrolling-active');
+
+      // Reset the timer every time the user scrolls
+      clearTimeout(timeout);
+
+      // Hide the scrollbar after 1.5 seconds of inactivity
+      timeout = setTimeout(() => {
+        document.body.classList.remove('scrolling-active');
+      }, 1500);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+    document.body.classList.remove('scrolling-active');
+  }, []);
+    
+  
+
   return (
     <BrowserRouter>
       <div className = "relative z-0 bg-primary">
